a9599b80ae24d1c7fd8ac3b48472f646aa5ed891
ozander <oliver.zander@gmail.com>
2013-03-06 16:20:50 +0100

    starting odertool 0.2

--- db/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = '85.214.202.153';
-$db = 'OrderTool';
-$user = 'ecommerce';
-$password = 'sFWe5ZWqfvTB4JKH';
-
-$con = mysql_connect($host, $user, $password);
-mysql_select_db($db);
-?>
--- mail/mail_functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function convert_mime_to_text($element) {
-	$data = array();
-	foreach (imap_mime_header_decode($element) as $part) {
-		$data[] = mb_convert_encoding($part->text, 'UTF-8');
-	}
-	return join(' ', $data);
-} 
-?>
