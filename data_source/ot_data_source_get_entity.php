c98bbcdb0d836e578a8b4d3663b824268c5c36d3
ozander <oliver.zander@gmail.com>
2013-03-06 17:28:33 +0100

    data source entity view

--- data_source/ot_data_source_get_entity.php
+++ data_source/ot_data_source_get_entity.php
@@ -1,14 +1,13 @@
-<?php
-
-$data_source = DataSource::get($_GET["id"]);
-$api_name = $data_source->api->name;
-
-?>
+<?php $data_source = DataSource::get($_GET["id"]); ?>
 
 <h1><?php echo $data_source->name; ?></h1>
 
-<?php echo $data_source->api->name; 
+<label for="api">API: </label>
+<select name="api">
+	<?php foreach (API::all() as $api): ?>
+		<?php $selected = ($api === $data_source->api) ? 'selected' : ''; ?>
+		<option value="<?php echo $api->id; ?>" <?php echo $selected; ?>><?php echo $api->name; ?></option>
+	<?php endforeach; ?>
+</select>
 
-	include "api/$api_name/main.php";
-
-?>
+<div class="api-main"><?php include 'api/' . $data_source->api->name . '/main.php'; ?></div>
