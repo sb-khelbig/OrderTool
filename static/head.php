24f3881194d25cfd7ad747dc279ecbb5894546bf
Konstantin Helbig <konstantin.helbig@salesbutlers.com>
2013-03-26 17:10:06 +0100

    quickselect

--- static/head.php
+++ static/head.php
@@ -3,3 +3,12 @@
 <link rel="stylesheet" type="text/css" href="static/layout.css">
 <script type="text/JavaScript" src="static/jquery-1.9.0.js" ></script>
 <script type="text/JavaScript" src="static/jquery-ui-1.10.0.custom.js" ></script>
+<script type="text/JavaScript" src="static/jquery.quickselect.js" ></script>
+<link rel="stylesheet" type="text/css" href="static/jquery.quickselect.css">
+<script>
+$(document).ready(
+		function()
+		{
+	$("select.quickselect").quickselect();
+});
+</script>
